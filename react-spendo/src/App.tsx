47456fdfb88276b936510e98c5a8de75832023fb
Fran Meglic <meglic.fran@gmail.com>
2025-08-03 08:32:18 +0200

    add: select transaction date range

--- react-spendo/src/App.tsx
+++ react-spendo/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Transactions from "./components/Transactions";
 import TransactionSummary from "./components/TransacionSummary";
 import TopTransactions from "./components/TopTransactions";
@@ -15,10 +15,15 @@ export interface Transaction {
 }
 
 function App() {
+	const dateFrom = useRef<HTMLInputElement>(null);
+	const dateTo = useRef<HTMLInputElement>(null);
+
 	const [transactions, updateTransactions] = useState<Transaction[]>([]);
+	const [dateRange, updateDateRange] = useState<String[]>(["2025-07-01", "2025-07-31"]);
+
 	const sendRequest = async () => {
-		const date_from = "2025-07-01";
-		const date_to = "2025-07-31";
+		const date_from = dateRange[0];
+		const date_to = dateRange[1];
 
 		const accountTransactionsUrl = `/api/v2/accounts/${ACCOUNT_ID}/transactions/?date_from=${date_from}&date_to=${date_to}`;
 		const options: RequestInit = {
@@ -47,11 +52,33 @@ function App() {
 
 	useEffect(() => {
 		sendRequest();
-	}, []);
+	}, [dateRange]);
+
+	const refreshHandler = () => {
+		if (dateFrom.current?.value !== "" && dateTo.current?.value !== "" && dateFrom.current && dateTo.current) {
+			const from = new Date(dateFrom.current.value);
+			const to = new Date(dateTo.current.value);
+			if (to.getTime() - from.getTime() <= 0) {
+				alert("Invalid date range!");
+				return;
+			}
+			updateDateRange([dateFrom.current.value, dateTo.current.value]);
+		}
+		console.log(dateRange[0]);
+		console.log(dateRange[1]);
+	};
 
 	return (
 		<>
-			<button onClick={sendRequest}>Refresh</button>
+			<div className="date-container">
+				<label>Date From:</label>
+				<input type="date" id="dateFrom" name="dateFrom" ref={dateFrom} />
+
+				<label>Date To:</label>
+				<input type="date" id="dateTo" name="dateTo" ref={dateTo} />
+
+				<button onClick={refreshHandler}>Refresh</button>
+			</div>
 			<div className="app-container">
 				<TransactionSummary transactions={transactions} />
 				<TopTransactions transactions={transactions} count={5} />
